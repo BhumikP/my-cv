742e5479232e8c9e0a52d8f74e20ea70c434d008
BhumikP <bhumik.bm123@gmail.com>
2022-02-22 09:29:48 +0530

    Added my resume

---
+++ components/constant.js
@@ -0,0 +1,103 @@
+export const myData = [
+  {
+    id: "1",
+    title: "intro",
+    body: (
+      <div>
+        <p>
+          I&apos;m currently working in Bacancy Technology as a Software
+          Developer with expertise in Javascript language.
+        </p>
+
+        <p>
+          I am passionate to start new things and ability to adapt changes and
+          have open mindset towards new approaches.
+        </p>
+      </div>
+    ),
+  },
+  {
+    id: "2",
+    title: "skills",
+    body: (
+      <div>
+        <p>
+          {" "}
+          <span className="text-md font-medium">LANGUAGES : </span> JavaScript
+          (ES6), HTML5, CSS3
+        </p>
+        <p>
+          {" "}
+          <span className="text-md font-medium">FRAMEWORKS : </span>
+          ReactJS,NextJS, NodeJS, MongoDB,GraphQL,Bootstrap
+        </p>
+        <p>
+          {" "}
+          <span className="text-md font-medium">TOOLS : </span> Git Github,
+          JIRA, Slack
+        </p>
+      </div>
+    ),
+  },
+  {
+    id: "3",
+    title: "education",
+    body: (
+      <div>
+        <p className="text-md font-normal">
+          Vishwakarma Govt. Engineering College
+          <span className="font-light">(2017 - 2021)</span>
+        </p>
+        <p className="text-md font-normal italic">Computer Engineering</p>
+      </div>
+    ),
+  },
+  {
+    id: "4",
+    title: "experience",
+    body: (
+      <div>
+        <p className="text-md font-normal">
+          Bacancy Technology{" "}
+          <span className="font-light">(Dec 2020 - Present)</span>
+        </p>
+        <p className="text-md font-normal italic">Software Developer</p>
+      </div>
+    ),
+  },
+  {
+    id: "5",
+    title: "projects",
+    body: (
+      <div>
+        <p className="text-xl font-normal">AMP</p>
+        <li>
+          Technologies used:ReactJS,Next JS,Rest API,MongoDB,GraphQL,Redux
+        </li>
+
+        <li>
+          My role was to build font-end parts,resposive view,API integration,Add
+          google analytics etc.
+        </li>
+        <li>
+          Using Next JS , we as team developed a static site AMP Community which
+          was mainly focused on increasing users to our site.
+        </li>
+
+        <p className="mt-5 font-normal text-xl">See Radio</p>
+        <li>Technologies used: ReactJS,MySQL</li>
+
+        <li>
+          See Radio is a system that mainly focuses on production, management &
+          distribution of the campaign (advertising video). It will produce a
+          campaign video (end product) based on client’s requirement.
+        </li>
+        <li>
+          I had worked on managing multi-step forms for adding orders and third
+          party organizations and viewing them all and API integration on the
+          front-end side.
+        </li>
+      </div>
+    ),
+  },
+];
